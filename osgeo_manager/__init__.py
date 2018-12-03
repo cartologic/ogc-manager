be6c99e38648c8b2cd1737f356b8a9a9e831acea
hisham waleed karam <hisham.karam@cartologic.com>
2018-12-03 13:03:19 +0200

    add apps.py

--- osgeo_manager/__init__.py
+++ osgeo_manager/__init__.py
@@ -6,3 +6,6 @@ DOWNLOAD_PATH = os.path.join(
     os.path.dirname(os.path.realpath(__file__)), 'downloads')
 create_direcotry(TEMP_PATH)
 create_direcotry(DOWNLOAD_PATH)
+
+
+default_app_config = 'osgeo_manager.apps.OSGEOManagerConfig'
---
+++ osgeo_manager/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class OSGEOManagerConfig(AppConfig):
+    name = 'osgeo_manager'
+    verbose_name = "osgeo_manager"
