4d6f7738bbf6091d7e184d1b47280222885b31ca
hisham waleed karam <hisham.karam@cartologic.com>
2018-12-06 17:50:46 +0200

    add requirements

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ long_description = open('README.md').read()
 setup(
     name='osgeo-manager',
     packages=find_packages(),
-    version='0.1.0',
+    version='0.2.0',
     description='Cartoview is a GIS web mapping application framework to \
     easily share and deploy apps based on Geonode',
     long_description=long_description,
@@ -26,7 +26,11 @@ setup(
     license="BSD",
     install_requires=['geonode>=2.8rc11',
                       'esridump==1.8.0',
-                      'python-sld @ https://github.com/gmioannou/python-sld/archive/master.zip',
-                      'ags2sld @ https://github.com/hishamkaram/ags2sld/archive/master.zip',
-                      ]
+                    #   'python-sld==0.1.0',
+                      'ags2sld==0.1.0'
+                      ],
+    dependency_links=[
+        'git+https://github.com/hishamkaram/ags2sld.git@master#egg=ags2sld-0.1.0.dev',
+        # 'git+https://github.com/gmioannou/python-sld.git@master#egg=python-sld-0.1.0.dev',
+    ],
 )
