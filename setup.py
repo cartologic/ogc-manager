f40675c9b1cc5bf11f01e1825d339509395d2011
Ahmed Nour Eldeen <ahmednoreldeen@gmail.com>
2021-03-15 17:09:58 +0200

    remove unused dependency

--- setup.py
+++ setup.py
@@ -24,12 +24,4 @@ setup(
         "Topic :: Scientific/Engineering :: GIS"
     ],
     license="BSD",
-    install_requires=['esridump==1.8.0',
-                      #   'python-sld==0.1.0',
-                      'ags2sld==0.1.0'
-                      ],
-    dependency_links=[
-        'git+https://github.com/hishamkaram/ags2sld.git@master#egg=ags2sld-0.1.0.dev',
-        # 'git+https://github.com/gmioannou/python-sld.git@master#egg=python-sld-0.1.0.dev',
-    ],
 )
